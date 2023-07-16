d31a6a690a2b073e22c29cc67f8b4bba3ce2af18
Austin Dumm <austin.dumm@gmail.com>
2023-07-16 16:13:44 -0400

    Add support for block comments with nesting

--- jlox/src/lox/Scanner.java
+++ jlox/src/lox/Scanner.java
@@ -12,22 +12,22 @@ class Scanner {
 
     static {
         keywords = new HashMap<>();
-        keywords.put("and",     TokenType.AND);
-        keywords.put("class",   TokenType.CLASS);
-        keywords.put("else",    TokenType.ELSE);
-        keywords.put("false",   TokenType.FALSE);
-        keywords.put("for",     TokenType.FOR);
-        keywords.put("fun",     TokenType.FUN);
-        keywords.put("if",      TokenType.IF);
-        keywords.put("nil",     TokenType.NIL);
-        keywords.put("or",      TokenType.OR);
-        keywords.put("print",   TokenType.PRINT);
-        keywords.put("return",  TokenType.RETURN);
-        keywords.put("super",   TokenType.SUPER);
-        keywords.put("this",    TokenType.THIS);
-        keywords.put("true",    TokenType.TRUE);
-        keywords.put("var",     TokenType.VAR);
-        keywords.put("while",   TokenType.WHILE);
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
     }
 
     private final String source;
@@ -116,6 +116,8 @@ class Scanner {
                 if (match('/')) {
                     while (peek() != '\n' && !isAtEnd())
                         advance();
+                } else if (match('*')) {
+                    blockComment();
                 } else {
                     addToken(TokenType.SLASH);
                 }
@@ -134,24 +136,56 @@ class Scanner {
 
     }
 
+    private void blockComment() {
+        blockComment(0);
+    }
+
+    private void blockComment(int depth) {
+        while (peek() != '*' && peek() != '/' && peek() != '\n')
+            advance();
+
+        if (peek() == '\n') {
+            advance();
+            line++;
+            blockComment(depth);
+       } else if (match('*') && peek() == '/') {
+            advance();
+            if (depth == 0) {
+                return;
+            } else {
+                blockComment(depth - 1);
+            }
+        } else if (match('/') && peek() == '*') {
+            advance();
+            blockComment(depth + 1);
+        } else {
+            advance();
+            blockComment(depth);
+        }
+    }
+
     private void identifier() {
-        while (isAlphaNumeric(peek())) advance();
+        while (isAlphaNumeric(peek()))
+            advance();
 
         String text = source.substring(start, current);
         TokenType type = keywords.get(text);
-        if (type == null) type = TokenType.IDENTIFIER;
+        if (type == null)
+            type = TokenType.IDENTIFIER;
 
         addToken(type);
     }
 
     private void number() {
-        while (isDigit(peek())) advance();
+        while (isDigit(peek()))
+            advance();
 
         if (peek() == '.' && isDigit(peekNext())) {
             // Consume '.'
             advance();
 
-            while (isDigit(peek())) advance();
+            while (isDigit(peek()))
+                advance();
         }
 
         addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
@@ -194,14 +228,15 @@ class Scanner {
     }
 
     private char peekNext() {
-        if (current + 1 >= source.length()) return '\0';
+        if (current + 1 >= source.length())
+            return '\0';
         return source.charAt(current + 1);
     }
 
     private boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') ||
-            (c >= 'A' && c <= 'Z') ||
-            c == '_';
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
     }
 
     private boolean isAlphaNumeric(char c) {
